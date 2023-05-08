a1de01a6c0292529bf341cc774ef831ec821fcdd
arifrahmadi23 <muhammad231arif@gmail.com>
2023-05-08 15:01:05 +0700

    membuat int main, member acces operator dan pointer

--- 171_Pointer/171_Pointer.cpp
+++ 171_Pointer/171_Pointer.cpp
@@ -12,3 +12,19 @@ void mahasiswa::showNim() { //implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
 }
 
+int main()
+{
+	mahasiswa mhs{ 1 };			// object mhs
+	mhs.showNim();				// Member Acces Operator
+
+	mahasiswa& ref = mhs;		// Pointer reference refMhs
+	ref.nim = 2;				// Member Access Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;		// Pointer Dereference pMhs
+	pMhs->nim = 3;				// Arrow Operator
+	mhs.showNim();
+	return 0;
+
+}
+
