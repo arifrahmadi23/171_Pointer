4126ac32a03b8dac25efe3d326b17bf01dc8fc55
arifrahmadi23 <muhammad231arif@gmail.com>
2023-05-08 15:33:46 +0700

    menambahakan beberapa method kedalam class

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -18,3 +18,17 @@ public:
 	void setId(int pId);
 	void displayAll(orang& a);
 };
+
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+
+void siswa::setId(int pId) {
+	id = pId;
+}
+
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
